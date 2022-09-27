85eb9d8fe3c10de4f535224bcca43e7d36377d2a
Amy Davies <amyloudav@gmail.com>
2022-09-27 15:26:37 +0200

    gatsby ssr set up

--- src/components/styles/global-styles.tsx
+++ src/components/styles/global-styles.tsx
@@ -1,21 +1,11 @@
 import styled from "styled-components";
 
-const size ={
-  tablet: '768px',
-  laptop: '1024px'
-}
+const size = {
+  tablet: "768px",
+  laptop: "1024px",
+};
 
 export const device = {
   tablet: `min-width: ${size.tablet}`,
-  laptop: `min-width: ${size.laptop}`
-}
-
-// export const GlobalStyles = styled.div`
-//   @import url("https://fonts.googleapis.com/css2?family=Signika:wght@300&display=swap");
-//   width: 100%;
-//   max-width: 100%;
-//   padding: 0;
-//   margin: 0 auto;
-//   color: #f25731;
-//   font-family: "Signika";
-// `;
+  laptop: `min-width: ${size.laptop}`,
+};
